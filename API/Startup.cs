e663eefd0c1201d87187be03c071cb65450eabb8
Jorge Martinez <jorge.martinez@sympatico.ca>
2021-12-24 13:33:54 -0500

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -24,7 +24,14 @@ namespace API
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
-        }
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
+        }            
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -38,6 +45,7 @@ namespace API
 
            // app.UseHttpsRedirection();
             app.UseRouting();
+            app.UseCors("CorsPolicy");
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
