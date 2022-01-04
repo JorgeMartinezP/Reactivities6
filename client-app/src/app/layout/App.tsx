a43efe38099f51c99aa81c47beba26d26cc493e3
Jorge Martinez <jorge.martinez@sympatico.ca>
2022-01-03 19:02:02 -0500

    Added basic routing.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,25 +1,21 @@
-import React, { useEffect } from 'react';
+import React  from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import LoadingComponent from './LoadingComponent';
-import useStore from '../stores/store';
 import { observer } from 'mobx-react-lite';
+import HomePage from '../../features/home/HomePage';
+import ActivityForm from '../../features/activities/Form/ActivityForm';
+import { Route } from 'react-router-dom';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 const App = () => {
-  const {activityStore} = useStore();
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  },[activityStore]);
-
-  if (activityStore.loadingInitial) return <LoadingComponent content='' />;
 
   return (
     <>
       <NavBar />
       <Container style={{marginTop: '7em' }}>
-        <ActivityDashboard />
+          <Route exact path='/' component={HomePage}/>
+          <Route path='/activities' component={ActivityDashboard} />
+          <Route path='/createActivity' component={ActivityForm} />
       </Container>
     </>
   );
