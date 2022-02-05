63496c713ffe5325a22e64fe3e1495af58c3e83d
Jorge Martinez <jorge.martinez@sympatico.ca>
2022-02-05 14:31:31 -0500

    Completed form and validations.

--- client-app/src/features/activities/Form/ActivityForm.tsx
+++ client-app/src/features/activities/Form/ActivityForm.tsx
@@ -1,17 +1,18 @@
 import { observer } from "mobx-react-lite";
-import React, { ChangeEvent, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useHistory, useParams } from "react-router-dom";
-import { Button, Label, Segment } from "semantic-ui-react";
+import { Button, Header, Segment } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import useStore from "../../../app/stores/store";
 import {v4 as uuid} from 'uuid';
-import { Formik, Form, ErrorMessage } from "formik";
+import { Formik, Form } from "formik";
 import * as Yup from 'yup';
 import MyTextInput from "../../../app/common/form/MyTextInput";
 import MyTextArea from "../../../app/common/form/MyTextArea";
 import MySelectInput from "../../../app/common/form/MySelectInput";
 import { categoryOptions } from "../../../app/common/options/categoryOptions";
 import MyDateInput from "../../../app/common/form/MyDateInput";
+import { Activity } from "../../../app/models/Activity";
 
 const ActivityForm = () => {
     const history = useHistory();
@@ -21,12 +22,12 @@ const ActivityForm = () => {
 
     const {id} = useParams<{id: string}>();
    
-    const [activity, setActivity] = useState({
+    const [activity, setActivity] = useState<Activity>({
         id: '',
         title: '',
         category: '',
         description: '',
-        date: '',
+        date: null,
         city: '',
         venue: ''   
     });
@@ -35,7 +36,7 @@ const ActivityForm = () => {
         title: Yup.string().required('The activity title is required'),
         description: Yup.string().required('The activity description is required'),
         category: Yup.string().required(),
-        date: Yup.string().required(),
+        date: Yup.string().required('Date is required').nullable(),
         venue: Yup.string().required(),
         city: Yup.string().required(),            
     })
@@ -44,37 +45,32 @@ const ActivityForm = () => {
         if (id) loadActivity(id).then(activity => setActivity(activity!));
     }, [id, loadActivity]);
 
-    // const handleSubmit = () => {
-    //     if (activity.id.length === 0) {
-    //         let newActivity = {
-    //             ...activity,
-    //             id: uuid()
-    //         };
-    //         createActivity(newActivity)
-    //           .then(() => history.push(`/activities/${newActivity.id}`));
-    //     }
-    //     else {
-    //         updateActivity(activity)
-    //            .then(() => history.push(`/activities/${activity.id}`));
-    //     }      
-    // }
-
-    // const handleInputChange =
-    //    (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-
-    //     const {name, value} = event.target;
-    //     setActivity({...activity, [name]: value});
-    // }
+    const handleFormSubmit = (activity: Activity) => {
+        if (activity.id.length === 0) {
+            let newActivity = {
+                ...activity,
+                id: uuid()
+            };
+            createActivity(newActivity)
+              .then(() => history.push(`/activities/${newActivity.id}`));
+        }
+        else {
+            updateActivity(activity)
+               .then(() => history.push(`/activities/${activity.id}`));
+        }      
+    }
 
     if (loadingInitial) return <LoadingComponent content='Loading activity...' />;
 
     return (
         <Segment clearing>
+            <Header content='Activity Details' sub color='teal' />
             <Formik 
                 validationSchema={validationSchema}
                 enableReinitialize
-                initialValues={activity} onSubmit={values => console.log(values)}>
-                {({ handleSubmit }) => (
+                initialValues={activity} 
+                onSubmit={values => handleFormSubmit(values)}>
+                {({ handleSubmit, isValid, isSubmitting, dirty }) => (
                 <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
                     <MyTextInput name='title' placeholder='Title' />
                     <MyTextArea rows={5} placeholder='Description' name='description' />
@@ -86,10 +82,17 @@ const ActivityForm = () => {
                          timeCaption='time'
                          dateFormat='MMMM d, yyyy h:mm aa'
                     />
+                    <Header content='Location Details' sub color='teal' />
                     <MyTextInput placeholder='City' name='city'/>
                     <MyTextInput placeholder='Venue' name='venue' />         
 
-                    <Button loading={loading} floated='right' positive type='submit' content='Submit'/>      
+                    <Button 
+                        disabled={isSubmitting || !dirty || !isValid}
+                        loading={loading} 
+                        floated='right' 
+                        positive type='submit' 
+                        content='Submit'
+                    />      
                     <Button as={Link} to='/activities' floated='right' type='button' content='Cancel'/>      
                 </Form>                
                 )} 
--- client-app/src/features/activities/details/ActivityDetailedHeader.tsx
+++ client-app/src/features/activities/details/ActivityDetailedHeader.tsx
@@ -1,3 +1,4 @@
+import { format } from 'date-fns';
 import { observer } from 'mobx-react-lite';
 import React from 'react'
 import { Link } from 'react-router-dom';
@@ -37,7 +38,7 @@ const ActivityDetailsHeader = ({activity}: Props) => {
                                     content={activity.title}
                                     style={{color: 'white'}}
                                 />
-                                <p>{activity.date}</p>
+                                <p>{format(activity.date!, 'dd MMM yyyy')}</p>
                                 <p>
                                     Hosted by <strong>Bob</strong>
                                 </p>
